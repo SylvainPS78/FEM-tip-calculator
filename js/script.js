5e45edfcc1a905201238571eec86988c2b51d825
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-03-22 11:56:13 +0700

    FIX Code improvment / refact

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-const billTotal = document.getElementById("bill-total");
+const billTotalInput = document.getElementById("bill-total");
 const tipBtns = document.querySelectorAll(".tip");
-const customTip = document.getElementById("custom-tip");
-const numberPeople = document.getElementById("number-people");
-const tipPerPerson = document.getElementById("tip-person");
-const totalPerPerson = document.getElementById("total-person");
+const customTipInput = document.getElementById("custom-tip");
+const numberPeopleInput = document.getElementById("number-people");
+const tipPerPersonOutput = document.getElementById("tip-person");
+const totalPerPersonOutput = document.getElementById("total-person");
 const resetBtn = document.getElementById("reset");
 const errorMessage = document.getElementById("error-message");
 
 
 // Initialisation
-tipPerPerson.textContent = "$0.00";
-totalPerPerson.textContent = "$0.00";
+tipPerPersonOutput.textContent = "$0.00";
+totalPerPersonOutput.textContent = "$0.00";
 let tipPerCent = 0;
 let peopleCount = 0;
 let billValue = 0;
@@ -20,70 +20,68 @@ const handleSubmit = (e) => {
     e.preventDefault();
 }
 
+// Calculations
+function calculateTipAmount() {
+    tipPerPersonOutput.textContent = (tipPerCent === 0 || peopleCount === 0) ? "$0.00" : `$${parseFloat((billValue*(tipPerCent)/peopleCount).toFixed(2))}`;
+}   
+
+function calculateTotalAmount() {
+    totalPerPersonOutput.textContent = (tipPerCent === 0 || peopleCount === 0) ? "$0.00" : `$${parseFloat((billValue*(1+tipPerCent)/peopleCount).toFixed(2))}`;
+}
+
 // Error message if Number of people = 0
 function errorDisplay() {
-    if (numberPeople.value === "0") {
-        numberPeople.style.border = "1px solid var(--Red)";
+    if (numberPeopleInput.value === "0") {
+        numberPeopleInput.style.border = "1px solid var(--Red)";
         errorMessage.style.display = "block";
     }
     else {
-        numberPeople.style.border = "1px solid var(--White)";
+        numberPeopleInput.style.border = "1px solid var(--White)";
         errorMessage.style.display = "none";
     }
-}
-
-// Calculations
-function calculateTip() {
-    tipPerPerson.textContent = (tipPerCent === 0 || peopleCount === 0) ? "$0.00" : `$${parseFloat((billValue*(tipPerCent)/peopleCount).toFixed(2))}`;
-}   
-
-function calculateTotal() {
-    totalPerPerson.textContent = (tipPerCent === 0 || peopleCount === 0) ? "$0.00" : `$${parseFloat((billValue*(1+tipPerCent)/peopleCount).toFixed(2))}`;
 }
 
 function reset() {
     tipPerCent = 0;
     peopleCount = 0;
     billValue = 0;
-    tipPerPerson.textContent = "$0.00";
-    totalPerPerson.textContent = "$0.00";
-    billTotal.value = "";
-    numberPeople.value = "";
-    customTip.value = "";
+    tipPerPersonOutput.textContent = "$0.00";
+    totalPerPersonOutput.textContent = "$0.00";
+    [billTotalInput, numberPeopleInput, customTipInput].forEach(input => input.value = "");
 }
 
-
+// Event handlers
 tipBtns.forEach(button => {
     button.addEventListener("click", (e) => {
         handleSubmit(e);
         tipPerCent = Number(button.value)/100;
-        if (!numberPeople.value || numberPeople.value == "0") {
+        if (!numberPeopleInput.value || numberPeopleInput.value == "0") {
             return
         }
         else {
-            calculateTip();
-            calculateTotal();
+            calculateTipAmount();
+            calculateTotalAmount();
         }
     });
 });
 
-customTip.addEventListener("input", () => {
-    tipPerCent = Number(customTip.value)/100;
-    calculateTip();
-    calculateTotal();
+customTipInput.addEventListener("input", () => {
+    tipPerCent = Number(customTipInput.value)/100;
+    calculateTipAmount();
+    calculateTotalAmount();
 })
 
-billTotal.addEventListener("input", ()=>{
-    billValue = Number(billTotal.value);
-    calculateTip();
-    calculateTotal();
+billTotalInput.addEventListener("input", ()=>{
+    billValue = Number(billTotalInput.value);
+    calculateTipAmount();
+    calculateTotalAmount();
 })
 
-numberPeople.addEventListener("input", () => {
+numberPeopleInput.addEventListener("input", () => {
     errorDisplay();
-    peopleCount = Number(numberPeople.value);
-    calculateTip();
-    calculateTotal();
+    peopleCount = Number(numberPeopleInput.value);
+    calculateTipAmount();
+    calculateTotalAmount();
 })
 
 resetBtn.addEventListener("click",reset)
